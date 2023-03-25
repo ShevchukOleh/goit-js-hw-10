a8c763f73b264f16a62a5d79f1d7dc0867d98eb4
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-03-25 09:06:36 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ function fetchCountries(countryName) {
     const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,flags,capital,population,languages`;
     return fetch(url).then(res => {
         if (!res.ok) {
-            throw new Error(res.statusText)
+            throw new Error(res.status)
         }
         return res.json()
     }).catch(err => {
